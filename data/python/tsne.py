84654fd19ca7bbc92415314cef0e457da8521502
Marc Rovira <marcrovirasacie@gmail.com>
2022-07-07 17:01:07 +0200

    Add data clening step to remove outliers

--- data/python/tsne.py
+++ data/python/tsne.py
@@ -2,6 +2,7 @@
 
 import keyfi as kf
 import pandas as pd
+import seaborn as sns
 
 from math import sqrt
 
@@ -11,16 +12,30 @@ df = pd.read_csv('database.csv')
 
 # %% Clean database
 
+# Remove input variables and run time output variable
+df.drop(labels=['runtime'], axis=1, inplace=True)
+
+# Remove rows with outliers in SL and delta base on IQR
+def remove_outlier_IQR(df):
+    Q1 = df.quantile(0.25)
+    Q3 = df.quantile(0.75)
+    IQR = Q3-Q1
+    df = df[~((df < (Q1-1.5*IQR)) | (df > (Q3+1.5*IQR)))]
+    return df
+
+df = remove_outlier_IQR(df)
+
+# Drop NaN values
 df.dropna(inplace=True)
+
+# Reindex dataframe
+df.reset_index(drop=True, inplace=True)
 
 # %% Run dimensionality reduction with t-SNE or UMAP
 
 # Find rounded sqrt of number of points
 N = len(df.index)
 n = round(sqrt(N))
-
-# Find a reasonable learning rate using default early_exaggeration=12
-learning_rate = max(N / 12 / 4, 50)
 
 # Use flag to change between algorithms
 flag_tsne = 1
@@ -30,9 +45,7 @@ if flag_tsne:
         data=df,
         algorithm=kf.dimred.TSNE,
         scale=True,
-        perplexity=n,
-        #learning_rate = learning_rate,
-        #init='pca'
+        perplexity=n
     )
 else:
     embedding, mapper = kf.embed_data(
@@ -51,7 +64,7 @@ kf.plot_embedding(
     embedding=embedding,
     data=df,
     scale_points=True,
-    cmap_var='phi',
+    cmap_var='SL',
     cmap_minmax=[]
 )
 
@@ -61,7 +74,6 @@ clusterer = kf.cluster_embedding(
     embedding=embedding,
     algorithm=kf.cluster.HDBSCAN,
     min_cluster_size=n,
-    min_samples=15,
     prediction_data=True
 )
 
